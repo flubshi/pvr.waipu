dfc120d1fb246ca064a2643b3b7d20715d788807
flubshi <flubshi@web.de>
2019-04-26 23:11:38 +0200

    fix: define timegm on windows

--- src/Utils.h
+++ src/Utils.h
@@ -6,6 +6,10 @@
 #include <string>
 #include <vector>
 
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#define timegm _mkgmtime
+#endif
+
 class Utils
 {
 public:
