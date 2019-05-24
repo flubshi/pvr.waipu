2a48ad08a82aec6902ea287671e517833fa52b7e
flubshi <flubshi@web.de>
2019-05-24 22:11:10 +0200

    Settings: Add check to verify requirements (widevine and network status)

---
+++ pvr.waipu/resources/check_requirements.py
@@ -0,0 +1,27 @@
+import xbmc
+import xbmcgui
+import inputstreamhelper
+import requests
+import json
+
+
+def getStatus():
+	url = "https://status.wpstr.tv/status?nw=wifi"
+	r = requests.get(url)
+	return r.json()
+
+is_helper = inputstreamhelper.Helper('mpd', drm='com.widevine.alpha')
+if is_helper.check_inputstream():
+	# widevine installed;
+	xbmc.log("[pvr.waipu] widevine: installed", level=xbmc.LOGDEBUG)
+	widevine_status = "OK - Widevine found."
+else:
+	# widevine not installed; notify user
+	xbmc.log("[pvr.waipu] widevine: not installed", level=xbmc.LOGDEBUG)
+	widevine_status = "Error - Not found!"
+	
+network_status_arr = getStatus()
+network_status = network_status_arr["statusText"]
+
+xbmcgui.Dialog().ok("pvr.waipu - Requirements", "[B]Widevine:[/B] " + widevine_status, "", "[B]Network status:[/B] " + network_status)
+
