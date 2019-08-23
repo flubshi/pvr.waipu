07fa7279e4e58d054e3a2a15cf12c74b228f4f74
Alwin Esch <alwin.esch@web.de>
2019-08-23 17:47:31 +0200

    change header include to start with "kodi/"
    This needed for reworked header checks by Kodi's addon build system.

--- src/client.h
+++ src/client.h
@@ -20,8 +20,8 @@
  *
  */
 
-#include "libXBMC_addon.h"
-#include "libXBMC_pvr.h"
+#include "kodi/libXBMC_addon.h"
+#include "kodi/libXBMC_pvr.h"
 
 extern bool                          m_bCreated;
 extern std::string                   g_strUserPath;
