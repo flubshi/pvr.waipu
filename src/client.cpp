d6af11b0bf05ca0c259ffdafa9fcb098a476e17f
Alwin Esch <alwin.esch@web.de>
2020-05-16 21:13:20 +0200

    api related updated

--- src/client.cpp
+++ src/client.cpp
@@ -86,7 +86,7 @@ extern "C"
     XBMC->Log(LOG_DEBUG, "End Readsettings");
   }
 
-  ADDON_STATUS ADDON_Create(void* hdl, void* props)
+  ADDON_STATUS ADDON_Create(void* hdl, const char* globalApiVersion, void* props)
   {
     if (!hdl || !props)
       return ADDON_STATUS_UNKNOWN;
